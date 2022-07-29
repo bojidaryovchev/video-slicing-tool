29376cc0d5ee6014fb416962ccf95c55283590cb
bojidaryovchev <blizzardcon10@gmail.com>
2022-07-29 18:46:03 +0300

    refactor: typings improvements

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,22 +1,18 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
-import { GET_VIDEO_ACTION } from './modules/store/actions/get-video.action';
-import { OPEN_FILE_DIALOG_ACTION } from './modules/store/actions/open-file-dialog.action';
-
-export type Channels = typeof OPEN_FILE_DIALOG_ACTION | typeof GET_VIDEO_ACTION;
 
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
-    sendMessage(channel: Channels, args: unknown[]) {
+    sendMessage(channel: string, args: unknown[]) {
       ipcRenderer.send(channel, args);
     },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
+    on(channel: string, func: (...args: unknown[]) => void) {
       const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
         func(...args);
       ipcRenderer.on(channel, subscription);
 
       return () => ipcRenderer.removeListener(channel, subscription);
     },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
+    once(channel: string, func: (...args: unknown[]) => void) {
       ipcRenderer.once(channel, (_event, ...args) => func(...args));
     },
   },
